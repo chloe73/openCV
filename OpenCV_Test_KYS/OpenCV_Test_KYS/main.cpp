fb03fadac3ada51480bffb9e6ecdd2878f3ee49d
Marvic1130 <marvic1130@gmail.com>
2020-12-31 21:12:47 +0900

    Image open&save test

--- OpenCV_Test_KYS/OpenCV_Test_KYS/main.cpp
+++ OpenCV_Test_KYS/OpenCV_Test_KYS/main.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
 #include "opencv2/highgui.hpp"
-#include "opencv2/core.hpp"
-#define IMAGE_PATH "/Users/admin/Desktop/Task/Project/opencvTest1/norazo/OpenCV_Test_KYS/OpenCV_Test_KYS/test.jpeg"
+
+#define IMAGE_PATH "test.jpeg"
 #define COLOR_IMAGE_WINDOW_NAME "color image"
 #define GRAY_IMAGE_WINDOW_NAME "gray image"
 
-int main(int argc, char** argv) {
+using namespace cv;
+using namespace std;
+int main(int argc, char* argv[]) {
+    cout << "OpenCV version : " << CV_VERSION << endl;
+    cout << "OpenCV major version : " << CV_MAJOR_VERSION << endl;
+    cout << "OpenCV minor version : " << CV_MINOR_VERSION << endl;
+    cout << "OpenCV subminor version : " << CV_SUBMINOR_VERSION << endl;
     
-    cv::Mat imageColor;
-    cv::Mat imageGray;
-
-    imageColor = cv::imread(IMAGE_PATH, cv::IMREAD_COLOR);
-    imageGray = cv::imread(IMAGE_PATH, cv::IMREAD_GRAYSCALE);
-
-    if (imageColor.empty() || imageGray.empty()){
-        std::cout << IMAGE_PATH
-            <<" 이미지를 불러오는 데 문제가 생겼습니다." << std::endl;
+    Mat Image = imread("test.jpeg");
+    if(Image.empty()){
+        cout << "Could not open find the image" << endl;
         return -1;
     }
-
-    cv::namedWindow(COLOR_IMAGE_WINDOW_NAME, cv::WINDOW_NORMAL);
-    cv::namedWindow(COLOR_IMAGE_WINDOW_NAME, cv::WINDOW_AUTOSIZE);
-    cv::imshow(COLOR_IMAGE_WINDOW_NAME, imageColor);
-    cv::imshow(GRAY_IMAGE_WINDOW_NAME, imageGray);
-
-    cv::waitKey(0);
-
-    cv::destroyWindow(COLOR_IMAGE_WINDOW_NAME);
-    cv::destroyWindow(GRAY_IMAGE_WINDOW_NAME);
-
+    
+    namedWindow("test", WINDOW_NORMAL);
+    namedWindow("test", WINDOW_AUTOSIZE);
+    imshow("test", Image);
+    waitKey(0);
+    
+    destroyWindow("test");
+    imwrite("output.png", Image);
     return 0;
 }
