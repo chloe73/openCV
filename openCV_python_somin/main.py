c249091bcd2935bf7f9a39a4cb090c412ba7acbf
chloe73 <esom735@gmail.com>
2021-01-21 01:31:35 +0900

    commit

--- openCV_python_somin/main.py
+++ openCV_python_somin/main.py
@@ -1,30 +1,30 @@
 # computer vision stduy : 이미지 뒤틀기(원근 변환 (perspective.py))
-import cv2
-import numpy as np
-
-file_name = "resource/111.jpg"
-img = cv2.imread(file_name)
-rows, cols = img.shape[:2]
-
-#---① 원근 변환 전 후 4개 좌표
-pts1 = np.float32([[0,0], [0,rows], [cols, 0], [cols,rows]])
-pts2 = np.float32([[100,50], [10,rows-50], [cols-100, 50], [cols-10,rows-50]])
-
-#---② 변환 전 좌표를 원본 이미지에 표시
-cv2.circle(img, (0,0), 10, (255,0,0), -1)
-cv2.circle(img, (0,rows), 10, (0,255,0), -1)
-cv2.circle(img, (cols,0), 10, (0,0,255), -1)
-cv2.circle(img, (cols,rows), 10, (0,255,255), -1)
-
-#---③ 원근 변환 행렬 계산
-mtrx = cv2.getPerspectiveTransform(pts1, pts2)
-#---④ 원근 변환 적용
-dst = cv2.warpPerspective(img, mtrx, (cols, rows))
-
-cv2.imshow("origin", img)
-cv2.imshow('perspective', dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# import cv2
+# import numpy as np
+#
+# file_name = "resource/111.jpg"
+# img = cv2.imread(file_name)
+# rows, cols = img.shape[:2]
+#
+# #---① 원근 변환 전 후 4개 좌표
+# pts1 = np.float32([[0,0], [0,rows], [cols, 0], [cols,rows]])
+# pts2 = np.float32([[100,50], [10,rows-50], [cols-100, 50], [cols-10,rows-50]])
+#
+# #---② 변환 전 좌표를 원본 이미지에 표시
+# cv2.circle(img, (0,0), 10, (255,0,0), -1)
+# cv2.circle(img, (0,rows), 10, (0,255,0), -1)
+# cv2.circle(img, (cols,0), 10, (0,0,255), -1)
+# cv2.circle(img, (cols,rows), 10, (0,255,255), -1)
+#
+# #---③ 원근 변환 행렬 계산
+# mtrx = cv2.getPerspectiveTransform(pts1, pts2)
+# #---④ 원근 변환 적용
+# dst = cv2.warpPerspective(img, mtrx, (cols, rows))
+#
+# cv2.imshow("origin", img)
+# cv2.imshow('perspective', dst)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 # computer vision stduy : 이미지 뒤틀기(어핀 변환 (getAffine.py))
